d28474955d71cc004b215c5e4e1864b26f7fc5b1
William Quintal <william95quintalwilliam@outlook.com>
2021-11-10 00:57:47 -0500

    add mqtt package and add dpkg armhf architecture

--- docker/baseRust.Dockerfile
+++ docker/baseRust.Dockerfile
@@ -1,10 +1,14 @@
 
 FROM ubuntu
 
+
+COPY docker/source.list /etc/apt/sources.list
+
 WORKDIR /usr/src/app
 
-RUN apt-get update && apt-get install -yq gcc-arm-linux-gnueabihf gcc
-RUN apt-get install -yq curl rsync openssh-client
+RUN dpkg --add-architecture armhf && \
+	apt-get update && apt-get upgrade && apt-get install -yq gcc-arm-linux-gnueabihf gcc
+RUN DEBIAN_FRONTEND="noninteractive"  TZ="America/New_York" apt-get install -yq curl rsync openssh-client pkg-config cmake
 
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 ENV PATH="/root/.cargo/bin:${PATH}"
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+/*use rumqtt::{MqttClient, MqttOptions, QoS};
+use std::{thread, time::Duration};
+
+fn main() {
+    let mqtt_options = MqttOptions::new("test-pubsub1", "localhost", 1883);
+    let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
+      
+    mqtt_client.subscribe("hello/world", QoS::AtLeastOnce).unwrap();
+    let sleep_time = Duration::from_secs(1);
+    thread::spawn(move || {
+        for i in 0..100 {
+            let payload = format!("publish {}", i);
+            thread::sleep(sleep_time);
+            mqtt_client.publish("hello/world", QoS::AtLeastOnce, false, payload).unwrap();
+        }
+    });
+
+    for notification in notifications {
+        println!("{:?}", notification)
+    }
+}*/
+
 fn main() {
     println!("Hello, world!");
 }
