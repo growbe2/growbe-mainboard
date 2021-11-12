980f29eb184c86897ec0860dfb44081e595650c0
William Quintal <william95quintalwilliam@outlook.com>
2021-11-12 14:59:35 -0500

    wip

--- src/comboard/imple/virt.rs
+++ src/comboard/imple/virt.rs
@@ -2,14 +2,46 @@
 use crate::comboard::imple::interface::{ModuleStateChangeEvent, ModuleValueValidationEvent};
 use std::sync::{Mutex, Arc, mpsc::Sender};
 
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+enum VirtualScenarioEvent {
+    VALUE = 1,
+    STATE = 2
+}
+
+#[derive(Serialize, Deserialize)]
+struct VirtualScenario {
+    pub eventType: VirtualComboardClient,
+    pub id: &'static str,
+    pub state: bool,
+    pub buffer: [u8; 512],
+}
+
+fn getConfig(config: &'static str) -> Result<()>  {
+    let configStr = std::fs::read_to_string("virtual-comboard.json")?;
+    let scenario = serde_json::from_str(&configStr.as_str())?;
+
+    println(scenario.id);
+
+    Ok(())
+}
+
 pub struct VirtualComboardClient {}
 
 impl super::interface::ComboardClient for VirtualComboardClient {
+
     fn run(&self,
         config: super::interface::ComboardClientConfig) -> tokio::task::JoinHandle<()> {
         return tokio::spawn(async move {
-            println!("Starting virtual truc mush");
-            config.senderStateChange.lock().unwrap().send(ModuleStateChangeEvent{
+
+            match getConfig("") {
+                Ok(_) => println!("OK"),
+                Err(_) => println!("Error")
+            }
+            // Read json config file
+           ;
+            /*config.senderStateChange.lock().unwrap().send(ModuleStateChangeEvent{
                 port: 1,
                 id: "AAP000000005",
                 state: true,
@@ -28,7 +60,7 @@ impl super::interface::ComboardClient for VirtualComboardClient {
                 port: 1,
                 id: "AAP000000005",
                 state: false,
-            }).unwrap();
+            }).unwrap();*/
         });
     }
 }
