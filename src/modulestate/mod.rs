8cace42f3e877873aa6572d1bf2e0d3783f41ee3
William Quintal <william95quintalwilliam@outlook.com>
2021-11-13 10:32:37 -0500

    module state

--- src/modulestate/mod.rs
+++ src/modulestate/mod.rs
@@ -3,16 +3,16 @@
 pub mod aap;
 pub mod interface;
 
-use crate::comboard::imple::interface::{ModuleStateChangeEvent, ModuleValueValidationEvent};
+use crate::{comboard::imple::interface::{ModuleStateChangeEvent, ModuleValueValidationEvent}, protos::message::GrowbeMessage};
 use std::collections::HashMap;
 
 struct MainboardConnectedModule {
     pub port: i32,
-    pub id: &'static str,
+    pub id: String,
 }
 
 struct MainboardModuleStateManager {
-    pub connectedModule: HashMap<String, MainboardConnectedModule>,
+    pub connected_module: HashMap<String, MainboardConnectedModule>,
 }
 
 
@@ -25,9 +25,11 @@ impl MainboardModuleStateManager {
 pub async fn moduleStateTask(
     receiverStateChange: std::sync::mpsc::Receiver<ModuleStateChangeEvent>,
     receiverValueValidation: std::sync::mpsc::Receiver<ModuleValueValidationEvent>,
+
+    senderSocket: std::sync::mpsc::Sender<GrowbeMessage>,
 ) {
-    let _manager = MainboardModuleStateManager{
-        connectedModule: HashMap::new(),
+    let mut manager = MainboardModuleStateManager{
+        connected_module: HashMap::new(),
     };
 
     loop {
@@ -35,6 +37,24 @@ pub async fn moduleStateTask(
             let receive = receiverStateChange.try_recv();
             if receive.is_ok() {
                 let state = receive.unwrap();
+
+                // a new module not connected is connected
+                if !manager.connected_module.contains_key(state.id.as_str()) {
+                    if state.state == true {
+                        manager.connected_module.insert(state.id.clone(), MainboardConnectedModule{
+                            port: state.port,
+                            id: state.id.clone(),
+                        });
+                    } else {
+                        // receive state disconnect for a module a didnt know was connected
+                    }
+                } else {
+                    if state.state == false {
+                        manager.connected_module.remove(state.id.as_str());
+                    } else {
+                        // state is true but was already connected , weird
+                    }
+                }
                 println!("Receive a state youpi {}", state.id);
             }
         }
