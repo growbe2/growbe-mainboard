f21ee3b9eded43d309e0a7d21cd2b3e3f5dfda81
William Quintal <william95quintalwilliam@outlook.com>
2021-11-13 01:32:34 -0500

    wip

--- src/comboard/imple/interface.rs
+++ src/comboard/imple/interface.rs
@@ -8,13 +8,13 @@ pub struct Module_Config {
 
 pub struct ModuleStateChangeEvent {
     pub port: i32,
-    pub id: &'static str,
+    pub id: String,
     pub state: bool,
 }
 
 pub struct ModuleValueValidationEvent {
     pub port: i32,
-    pub buffer: [u8; 512],
+    pub buffer: Vec<u8>,
 }
 
 pub struct ComboardClientConfig {
--- src/comboard/imple/virt.rs
+++ src/comboard/imple/virt.rs
@@ -5,26 +5,38 @@ use std::sync::{Mutex, Arc, mpsc::Sender};
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
-enum VirtualScenarioEvent {
-    VALUE = 1,
-    STATE = 2
+
+fn defaultIndex() -> i32 {
+    return -1;
+}
+
+#[derive(Serialize, Deserialize)]
+struct VirtualScenarioItem {
+    pub eventType: String,
+    #[serde(default)] 
+    pub port: i32,
+    #[serde(default)] 
+    pub id: String,
+    #[serde(default)] 
+    pub state: bool,
+    #[serde(default)] 
+    pub buffer: Vec<u8>,
+    #[serde(default = "defaultIndex")] 
+    pub returnIndex: i32,
+    #[serde(default)] 
+    pub timeout: u64, // in milliseconds
 }
 
 #[derive(Serialize, Deserialize)]
 struct VirtualScenario {
-    pub eventType: VirtualComboardClient,
-    pub id: &'static str,
-    pub state: bool,
-    pub buffer: [u8; 512],
+    pub actions: Vec<VirtualScenarioItem>,
 }
 
-fn getConfig(config: &'static str) -> Result<()>  {
-    let configStr = std::fs::read_to_string("virtual-comboard.json")?;
-    let scenario = serde_json::from_str(&configStr.as_str())?;
 
-    println(scenario.id);
-
-    Ok(())
+fn getConfig(config: &'static str) -> Result<VirtualScenario>  {
+    let file = std::fs::File::open(config).expect("Error open file");
+    let scenario: VirtualScenario = serde_json::from_reader(file)?;
+    Ok(scenario)
 }
 
 pub struct VirtualComboardClient {}
@@ -32,35 +44,48 @@ pub struct VirtualComboardClient {}
 impl super::interface::ComboardClient for VirtualComboardClient {
 
     fn run(&self,
-        config: super::interface::ComboardClientConfig) -> tokio::task::JoinHandle<()> {
+        configComboard: super::interface::ComboardClientConfig) -> tokio::task::JoinHandle<()> {
         return tokio::spawn(async move {
-
-            match getConfig("") {
-                Ok(_) => println!("OK"),
-                Err(_) => println!("Error")
-            }
+            let config = getConfig("./virtual-comboard.json").expect("Failed to load config for virtual comboard");
             // Read json config file
-           ;
-            /*config.senderStateChange.lock().unwrap().send(ModuleStateChangeEvent{
-                port: 1,
-                id: "AAP000000005",
-                state: true,
-            }).unwrap();
+            let mut i: usize = 0;
+            while i < config.actions.len() {
+                match config.actions[i].eventType.as_str() { 
+                    "state" => {
+                        configComboard.senderStateChange.lock().unwrap().send(
+                            ModuleStateChangeEvent{
+                                port: config.actions[i].port,
+                                id: config.actions[i].id.clone(),
+                                state: config.actions[i].state,
+                            }
+                        );
+                    },
+                    "value" => {
+                        let newBuffer = config.actions[i].buffer.clone();
+                        configComboard.senderValueValidation.lock().unwrap().send(
+                            ModuleValueValidationEvent{
+                                port: config.actions[i].port,
+                                buffer: newBuffer,
+                            }
+                        );
+                    },
+                    _ => {
+                        println!("Invalid event type for action");
+                    }
+                }
 
-            tokio::time::sleep(tokio::time::Duration::from_millis(2000)).await;
+                if config.actions[i].timeout > 0 {
+                    tokio::time::sleep(tokio::time::Duration::from_millis(config.actions[i].timeout)).await;
+                }
 
-            config.senderValueValidation.lock().unwrap().send(ModuleValueValidationEvent{
-                port: 5,
-                buffer: [5; 512],
-            });
+                if config.actions[i].returnIndex > -1 {
+                    i = config.actions[i].returnIndex as usize; 
+                } else {
+                    i += 1;
+                }
+            }
 
-            tokio::time::sleep(tokio::time::Duration::from_millis(2000)).await;
-
-            config.senderStateChange.lock().unwrap().send(ModuleStateChangeEvent{
-                port: 1,
-                id: "AAP000000005",
-                state: false,
-            }).unwrap();*/
+            println!("End of current scenario virtual comboard");
         });
     }
 }
