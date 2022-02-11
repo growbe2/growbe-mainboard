660c197924627bb1589f68ac06193eb819da3968
William Quintal <wquintal@alborea.com>
2022-02-10 20:06:27 -0500

    gc-158, fixe alarme minuit < closing time

--- src/modulestate/relay/alarm.rs
+++ src/modulestate/relay/alarm.rs
@@ -1,14 +1,38 @@
-use crate::protos::module::{AlarmConfig};
+use std::thread::current;
+
+use crate::protos::module::AlarmConfig;
 use chrono::Timelike;
 use tokio::select;
 use tokio_util::sync::CancellationToken;
+
+fn get_current_min() -> i32 {
+    let curr_time = crate::mainboardstate::rtc::get_rtc().time();
+    return (curr_time.hour() * 60 + curr_time.minute()) as i32;
+}
+
+fn get_timeout_alarm(current_mins: i32, day_mins: i32, night_mins: i32) -> (i32, bool) {
+    return if night_mins < day_mins {
+        if current_mins < night_mins {
+            (night_mins - current_mins, true)
+        } else if current_mins >= night_mins && current_mins < day_mins {
+            (day_mins - current_mins, false)
+        } else {
+            ((1500 - current_mins) + night_mins, true)
+        }
+    } else if current_mins < day_mins {
+        ((day_mins - current_mins), false)
+    } else if current_mins < night_mins {
+        ((night_mins - current_mins), true)
+    } else {
+        ((((24 * 60) - current_mins) + day_mins), false)
+    }
+}
 
 pub fn set_alarm_relay(
     relay: &mut impl super::Relay,
     config: &AlarmConfig,
     cancellation_token: CancellationToken,
 ) -> tokio::task::JoinHandle<()> {
-
     let beginning = config.begining.as_ref().unwrap();
     let ending = config.end.as_ref().unwrap();
 
@@ -18,34 +42,32 @@ pub fn set_alarm_relay(
     let hr_night = ending.hour;
     let min_night = ending.minute;
 
-    let day_mins = hr_day*60+min_day;
-    let night_mins = hr_night*60+min_night;
-
+    let day_mins = hr_day * 60 + min_day;
+    let night_mins = hr_night * 60 + min_night;
 
     let mut relay = relay.clone_me();
-   
     return tokio::spawn(async move {
-        log::debug!("starting alarm process for port {}, day_mins {} night_mins {}", relay.id(), day_mins, night_mins);
+        log::debug!(
+            "starting alarm process for port {}, day_mins {} night_mins {}",
+            relay.id(),
+            day_mins,
+            night_mins
+        );
 
         // TODO : make a better implementation , this will cause issue on clock update, hour change
+        // 1500 === 0
         loop {
-            let curr_time = crate::mainboardstate::rtc::get_rtc().time();
-            let current_minute: i32 = (curr_time.hour() * 60 + curr_time.minute()) as i32;
 
-            let (timeout, day) = if current_minute < day_mins {
-                ((day_mins - current_minute), false)
-            } else if current_minute < night_mins {
-                ((night_mins - current_minute), true)
-            } else if current_minute >= night_mins {
-               ((((24 * 60) - current_minute) + day_mins), false)
-            } else {
-                panic!("impossibru")
-            };
-
+            let current_minute = get_current_min();
+            let (timeout, day) = get_timeout_alarm(current_minute, day_mins, night_mins);
             let value = if day { 1 } else { 0 };
             relay.set_state(value).unwrap();
 
-            log::debug!("have to sleep for {} minute(s) from {}", timeout, current_minute);
+            log::debug!(
+                "have to sleep for {} minute(s) from {}",
+                timeout,
+                current_minute
+            );
 
             select! {
                 _ = cancellation_token.cancelled() => {
@@ -58,4 +80,30 @@ pub fn set_alarm_relay(
             }
         }
     });
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test] 
+    fn valid_alarm_values() {
+        let values: Vec<(i32,i32,i32,i32,bool)> = vec![
+            // OPEN , CLOSE , CURRENT , EXPECTED SLEEP , IS_DAY
+            (600, 800, 700, 100, true),
+            (360, 0, 800, 700, true),
+            (360, 0, 100, 260, false),
+            (360, 100, 800, 800, true)
+        ];
+
+        for value in values.iter() {
+            let (timeout, day) = get_timeout_alarm(value.2, value.0, value.1);
+
+            assert_eq!(timeout, value.3);
+            assert_eq!(day, value.4);
+        }
+   }
+
 }
