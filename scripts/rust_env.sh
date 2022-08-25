5c30158de08e7a7a3db33aedc6832f0dff199e01
William Quintal <william95quintalwilliam@outlook.com>
2022-08-25 14:58:54 -0400

    update and fix docker image for build

--- scripts/rust_env.sh
+++ scripts/rust_env.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --rm --env COMMIT="$COMMIT" -v "$PWD":/usr/src/app -w /usr/src/app docker.pkg.github.com/growbe2/growbe-mainboard/dev "$@"
+docker run --rm --env COMMIT="$COMMIT" --env PKG_CONFIG_SYSROOT_DIR=/usr/arm-linux-gnueabihf/ -v "$PWD":/usr/src/app -w /usr/src/app docker.pkg.github.com/growbe2/growbe-mainboard/dev "$@"
