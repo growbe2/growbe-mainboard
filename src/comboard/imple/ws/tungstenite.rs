b19e731d1feaaf9dc5abc0ec75f31dccc86d1cf8
William Quintal <william95quintalwilliam@outlook.com>
2022-09-01 18:22:13 -0400

    wip

--- src/comboard/imple/ws/tungstenite.rs
+++ src/comboard/imple/ws/tungstenite.rs
@@ -1,35 +1,66 @@
 use std::sync::mpsc::Receiver;
 
-use futures::TryStreamExt;
+use futures::{future, pin_mut, Future, TryStreamExt};
 use futures_util::StreamExt;
+use serde::{Deserialize, Serialize};
 use tokio::io::AsyncWriteExt;
 use tokio_tungstenite::connect_async;
 use url::Url;
+
+use crate::comboard::imple::channel::ModuleConfig;
+
+#[derive(Serialize, Deserialize)]
+pub struct WebSocketMessage {
+    pub topic: String,
+    pub data: String,
+}
+
+const TOPIC_MODULE_ID: &str = "READ_MODULE_ID";
+const TOPIC_MODULES: &str = "READ_SUPPORTED_MODULES";
+const TOPIC_MODULE_DATA_PREFIX: &str = "DATA:(.*)";
 
 fn parse_client_addr(config: String) -> Vec<String> {
     return config.split(";").map(|x| x.to_string()).collect();
 }
 
-async fn handle_device_loop(url: Url) -> Result<(), ()> {
-    let (ws_stream, _) = connect_async(url)
-        .await
-		.map_err(|_| ())?;
+async fn handle_device_loop(url: Url, receiver: Receiver<ModuleConfig>) -> Result<(), ()> {
+    let (ws_stream, _) = connect_async(url).await.map_err(|_| ())?;
     println!("WebSocket handshake has been successfully completed");
 
     let (write, read) = ws_stream.split();
 
-    //let ws_to_stdout = {
-    read.try_for_each(|message| async {
-		let data = message.into_data();
+    let mut module_id: String = "".to_string();
+    let mut supported_modules: Vec<String> = vec![];
 
-        // Send data to comboard manager
-		tokio::io::stdout().write_all(&data).await.unwrap();
-		Ok(())
-    })
-    .await.map_err(|_| ())?;
-    //};
+    let task_read = {
+        read.try_for_each(|message| async {
+            let data = message.into_data();
+            if let Ok(message) = serde_json::from_slice::<WebSocketMessage>(&data) {
+                match message.topic.as_str() {
+                    TOPIC_MODULE_ID => {}
+                    TOPIC_MODULES => {}
+                    _ => {
+                        // Module data
+                    }
+                }
+            }
 
-	Ok(())
+            Ok(())
+        })
+    };
+
+    let task_write = async {
+        loop {
+            if let Ok(config) = receiver.recv() {
+                println!("config to send to ws");
+            }
+        }
+    };
+
+    pin_mut!(task_read, task_write);
+    future::select(task_read, task_write).await;
+
+    return Ok(());
 }
 
 pub struct WSComboardClient {
@@ -37,7 +68,10 @@ pub struct WSComboardClient {
 }
 
 impl crate::comboard::imple::interface::ComboardClient for WSComboardClient {
-    fn run(&self, receiver_config: Receiver<crate::comboard::imple::channel::ModuleConfig>) -> tokio::task::JoinHandle<()> {
+    fn run(
+        &self,
+        receiver_config: Receiver<crate::comboard::imple::channel::ModuleConfig>,
+    ) -> tokio::task::JoinHandle<()> {
         let config =
             ("ws://".to_string() + &self.config_comboard.config.clone() + ":5000/live").to_string();
 
@@ -45,16 +79,16 @@ impl crate::comboard::imple::interface::ComboardClient for WSComboardClient {
 
         println!("{:?}", url);
 
-        return tokio::spawn(async move {
+        tokio::spawn(async move {
             loop {
-				match handle_device_loop(url.clone()).await {
-					Ok(_) => {},
-					Err(_) => {
-						log::warn!("failed to connect");
-						tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
-					}
-				}
-			}
-        });
+                match handle_device_loop(url.clone(), receiver_config).await {
+                    Ok(_) => {}
+                    Err(_) => {
+                        log::warn!("failed to connect");
+                        tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
+                    }
+                }
+            }
+        })
     }
 }
