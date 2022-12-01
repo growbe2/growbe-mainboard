a86a3e91a5f49da3fdd1027bbd85df91b87af1ac
William Quintal <william95quintalwilliam@outlook.com>
2022-11-30 19:46:19 -0500

    add cache to vendors

--- docker/scripts/coverage.sh
+++ docker/scripts/coverage.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 mkdir -p ./target/coverage/html
-grcov . --binary-path ./target/debug/deps -s . -t html --branch --ignore-not-existing --ignore '*vendor*' -o target/coverage/html
-grcov . --binary-path ./target/debug/deps -s . -t cobertura --branch --ignore-not-existing --ignore '*vendor*' -o target/coverage/cobertura.xml
+grcov . --binary-path ./target/cov/debug/deps -s . -t html --branch --ignore-not-existing --ignore '*vendor*' -o target/coverage/html
+grcov . --binary-path ./target/cov/debug/deps -s . -t cobertura --branch --ignore-not-existing --ignore '*vendor*' -o target/coverage/cobertura.xml
